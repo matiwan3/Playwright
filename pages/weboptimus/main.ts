95c332f8cf85f38d166ce887cb2dfdda559c215a
Mateusz <wanmatedzlewicz@gmail.com>
2025-01-10 03:11:33 +0100

    adjusted tests and POM

---
+++ pages/weboptimus/main.ts
@@ -0,0 +1,13 @@
+import { Page } from 'playwright';
+
+export class MainPage {
+    public page: Page;
+    public heading: any;
+    public uniqueValues: any;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.heading = page.getByRole('heading', { name: 'Nowoczesne strony internetowe', exact: true });
+        this.uniqueValues = page.getByRole('heading', { name: 'Co nas wyróżnia?' });
+    }
+}
--- tests/weboptimus.spec.ts
+++ tests/weboptimus.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { MainPage } from '../pages/weboptimus/main';
 
 test.describe('Set of tests for weboptimus webpage', () => {
     test.beforeEach(async ({ page }) => {
@@ -9,6 +10,8 @@ test.describe('Set of tests for weboptimus webpage', () => {
         await page.close();
     });
     test('Check if the page has a title', async ({ page }) => {
+        const mainPage = new MainPage(page);
         await expect(page).toHaveTitle("Weboptimus – Tworzenie stron WWW");
+        expect(await mainPage.heading.isVisible()).toBeTruthy();
     });
 });
